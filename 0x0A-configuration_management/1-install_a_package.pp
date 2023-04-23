020217b1e1ffa5345941f4d8e42bf2eb0dd09646
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-04-23 20:13:13 +0000

    Install package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,13 @@
 # This Puppet file Install Flask on servers
 
-service { 'pip install Flask':
-    ensure  => 2.1.0
+#service { 'pip install Flask':
+#    ensure  => 2.1.0
+#}
+
+package { 'python3-pip':
+  ensure => installed,
+}
+exec { 'install_flask':
+    command => '/usr/bin/pip3 install flask==2.1.0',
+    creates => '/usr/local/lib/python3.8/dist-packages/flask',
 }
