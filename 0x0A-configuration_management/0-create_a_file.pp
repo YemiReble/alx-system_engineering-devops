05e5df177ac093192b11add16225b604d24bd8d0
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-04-23 18:46:12 +0000

    Create File

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,8 +2,9 @@
 # With some contents in it.
 
 file {'tmp/school':
-    mode => '0744',
-    owner => 'www-data',
-    group => 'www-data',
+    ensure  => present,
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
     content => "I love Puppet\n",
 }
