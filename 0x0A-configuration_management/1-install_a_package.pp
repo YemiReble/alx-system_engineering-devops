362453772ae9d712f456cc5ace92367f19501a39
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-04-23 19:49:16 +0000

    Install package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
 # This Puppet file Install Flask on servers
 
-service { pip install Flask:
-    ensure  => installed
+# service { pip install Flask:
+#    ensure  => installed
+#}
+
+package { flask:
+    ensure      => installed,
+    provider    => pip,
 }
