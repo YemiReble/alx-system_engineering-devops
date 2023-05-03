74a496620fd998c9558bad4a176bac5ca92d1b5f
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-05-03 15:50:09 +0000

    Configure A New Ubuntu Server (This Server will have an Header X-Server-By it hostname

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,24 +1,23 @@
 # Puppet That configure a new server with a Header
 
 # Making Sure Nginx is available on the server.
-package { "nginx":
+package { 'nginx':
     ensure  =>  present
 }
 
 # Install Nginx
-exec { "install":
-    command     =>  "sudo apt-get update ; sudo apt-get install nginx -y",
-    provider    =>  shell
+exec { 'install':
+    command =>  'sudo apt-get update ; sudo apt-get install nginx -y'
 }
 
 # Add Header and Redirection code to the Exisiting commands in site enabled
-file_line { "http_header":
-    path    =>  /etc/nginx/sites-enabled/default,
-    match   =>  'http{',
-    line    =>  "http{\n\tadd_header X-Server-By \"${hostname}\";",
+-> file_line { 'http_header':
+    path  => '/etc/nginx/sites-enabled/default',
+    match => 'http{',
+    line  => "http{\n\tadd_header X-Server-By \"${hostname}\";",
 }
 
 # Restart Nginx
-exec { "run":
-    command     =>  "sudo service nginx restart"
-    provider    => shell
+exec { 'run':
+    command =>  'sudo service nginx restart'
+}
