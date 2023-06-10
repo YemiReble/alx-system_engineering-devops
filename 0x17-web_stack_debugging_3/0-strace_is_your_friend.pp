ae0ad7b62ffc99f3c93edca94c046e381ac41aef
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-06-10 10:40:23 +0000

    Fixing Apache2 Server Error [500]

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,22 +1,6 @@
 # Fix the Apache2 Server that is returning 500 error
 
-service { 'apache2':
-  ensure => running,
-  enable => true,
-}
-
-package { 'php5':
-  ensure => installed,
-}
-
-file { '/etc/php5/apache2/php.ini':
-  ensure  => file,
-  content => template('path/to/php.ini.erb'),
-  require => Package['php5'],
-  notify  => Service['apache2'],
-}
-
-$file2 = '/var/www/html/wp-settings.php'
+$file2 = '/var/www/html/wp-settings.php'wp-content
 
 # Replace the line containing the error "phpp" with "php"
 
