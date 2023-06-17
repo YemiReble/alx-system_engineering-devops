08680d9a287cafffff0079671cadb46c1aa81441
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-06-17 13:21:53 +0000

    Change os configuration for holberton user

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,29 @@
-# This puppet script allow user 'holberton' to login and open files without error
-
-# Increase hard file limit for user 'holberton'
-exec { 'change-os-configuration-for-holberton-user':
-    command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
-    path    => '/usr/local/bin/:/bin/'
+# Create the "holberton" user
+user { 'holberton':
+  ensure     => 'present',
+  managehome => true,
 }
 
-# Increase soft file limit for user 'holberton'
-exec { 'change-os-configuration-for-holberton-user':
-    command => 'sed -i "/holberton soft/s/5/50000/" /etc/security/limits.conf',
-    path    => '/usr/local/bin/:/bin/'
+# Allow the "holberton" user to open files without error messages
+file { '/etc/security/limits.conf':
+  ensure  => 'file',
+  content => "holberton soft nofile 65536\nholberton hard nofile 65536",
+  mode    => '0644',
+  require => User['holberton'],
+}
+
+# Allow the "holberton" user to login
+file { '/etc/ssh/sshd_config':
+  ensure  => 'file',
+  content => 'AllowUsers holberton',
+  mode    => '0644',
+  require => User['holberton'],
+  notify  => Service['sshd'],
+}
+
+# Restart the SSH service
+service { 'sshd':
+  ensure  => 'running',
+  enable  => true,
+  require => File['/etc/ssh/sshd_config'],
 }
