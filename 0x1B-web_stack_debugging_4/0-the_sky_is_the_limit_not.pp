72ae45383fec657da7346124a534e89c31cc8513
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-06-17 10:56:59 +0000

    Reconfiguring Nginx So THat It can Process 1000 Requests Per 100 Requests

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,36 +1,13 @@
 # A Puppet file that fixes Nginx to accept requests
 
-# Class web_stack connections
-class web_stack {
-  # Increase the maximum number of concurrent connections
-  sysctl { 'net.core.somaxconn':
-    value => '1024',
-  }
-
-  # Adjust the maximum number of file descriptors
-  limits::fragment { 'web_stack':
-    content => "www-data soft nofile 65536\nwww-data hard nofile 65536",
-    order   => '10',
-  }
-
-  # Configure Apache or Nginx to handle more concurrent connections
-  # Example for Apache:
-  class { 'apache':
-    mpm_module  =>  'event',
-  }
-  apache::mod { 'mpm_event':
-    ensure  =>  'present',
-  }
-  apache::mpm { 'event':
-    serverlimit            => '100',
-    maxrequestworkers      => '1000',
-    maxconnectionsperchild => '100',
-  }
-
-  # Example for Nginx:
-  class { 'nginx':
-    worker_processes   => 'auto',
-    worker_connections => '1024',
-  }
+# Reconfigure ULIMIT='-n 4096'
+exec { 'config_nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
 }
 
+# Restart Nginx
+-> exec { 'restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
