3aec2f53d8d1fd32e9aacaf11cc215564bd04e4c
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-06-17 10:40:54 +0000

    Version Update

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,19 +1,36 @@
-# A Puppet file that fixes Nginx Error
+# A Puppet file that fixes Nginx to accept requests
 
-class fix_nginx {
-
-  file { '/etc/nginx/conf.d/fix-nginx.conf':
-    ensure  => file,
-    owner   => root,
-    group   => root,
-    mode    => 0644,
-    source  => 'puppet:///modules/fix_nginx/fix-nginx.conf',
+# Class web_stack connections
+class web_stack {
+  # Increase the maximum number of concurrent connections
+  sysctl { 'net.core.somaxconn':
+    value => '1024',
   }
 
-  service { 'nginx':
-    ensure  => running,
-    enable  => true,
-    restart => true,
+  # Adjust the maximum number of file descriptors
+  limits::fragment { 'web_stack':
+    content => "www-data soft nofile 65536\nwww-data hard nofile 65536",
+    order   => '10',
   }
 
+  # Configure Apache or Nginx to handle more concurrent connections
+  # Example for Apache:
+  class { 'apache':
+    mpm_module  =>  'event',
+  }
+  apache::mod { 'mpm_event':
+    ensure  =>  'present',
+  }
+  apache::mpm { 'event':
+    serverlimit            => '100',
+    maxrequestworkers      => '1000',
+    maxconnectionsperchild => '100',
+  }
+
+  # Example for Nginx:
+  class { 'nginx':
+    worker_processes   => 'auto',
+    worker_connections => '1024',
+  }
 }
+
